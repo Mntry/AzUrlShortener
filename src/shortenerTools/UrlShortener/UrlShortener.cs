28423fbe4e872fef6d8a1bb1d2e2398cf9346e08
Jason Brady <jbrady@octopi.com>
2021-03-25 17:40:22 -0700

    feature: update security

--- src/shortenerTools/UrlShortener/UrlShortener.cs
+++ src/shortenerTools/UrlShortener/UrlShortener.cs
@@ -39,10 +39,9 @@ namespace Cloud5mins.Function
 
     public static class UrlShortener
     {
-        //update auth level
         [FunctionName("UrlShortener")]
         public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
         ILogger log,
         ExecutionContext context,
         ClaimsPrincipal principal)
